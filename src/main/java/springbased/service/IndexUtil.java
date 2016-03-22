c3ecbfa7fdd3bb689a60bf800accca3403a29acd
Zonghan Wu <a476678244@gmail.com>
2016-03-22 14:12:32 +0800

    fix copy function based index

--- src/main/java/springbased/service/IndexUtil.java
+++ src/main/java/springbased/service/IndexUtil.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import org.apache.log4j.Logger;
 
@@ -24,10 +25,10 @@ public class IndexUtil {
       pstmt = sourceConn.prepareStatement(
           " select table_name,index_name,COLUMN_NAME from " + "("
               + "select dic.table_name,dic.index_name,dic.COLUMN_NAME ,dic.COLUMN_POSITION from dba_indexes  di, "
-              + "dba_ind_columns dic where upper(di.owner)=? and di.uniqueness='UNIQUE' and "
+              + "dba_ind_columns dic where upper(di.owner)=?  and "
               + "di.owner=dic.index_owner and di.index_name=dic.index_name "
               + "and not exists (select 1 from dba_constraints dc where dc.owner=di.owner and dc.CONSTRAINT_NAME=di.index_name and dc.CONSTRAINT_TYPE='P') "
-              + "and index_type='NORMAL' ) order by table_name,index_name, COLUMN_POSITION");
+              + "and index_type in ('NORMAL', 'FUNCTION-BASED NORMAL') ) order by table_name,index_name, COLUMN_POSITION");
       pstmt.setString(1, sourceSchema.toUpperCase());
       // pstmt.setString(2, sourceSchema);
       ResultSet rs = pstmt.executeQuery();
@@ -38,6 +39,10 @@ public class IndexUtil {
         String tabName = rs.getString(1);
         String indName = rs.getString(2);
         String colName = rs.getString(3);
+        if (colName.toUpperCase().contains("SYS_NC")) {
+          colName = translateSysNcToTableName(sourceSchema, colName, targetConn,
+              sourceConn, tabName);
+        }
         tab2Ind.put(indName, tabName);
         log.info("table vs index found:" + tabName + " vs " + indName);
         List<String> colList = cols2Ind.get(indName);
@@ -46,32 +51,6 @@ public class IndexUtil {
           cols2Ind.put(indName, colList);
         }
         colList.add(colName);
-      }
-      rs.close();
-      pstmt.close();
-      pstmt = sourceConn.prepareStatement(
-          " select table_name,index_name,COLUMN_NAME from " + "("
-              + "select dic.table_name,dic.index_name,dic.COLUMN_EXPRESSION as column_name ,dic.COLUMN_POSITION  from dba_indexes  di,"
-              + "DBA_IND_EXPRESSIONS dic where upper(di.owner)=? and di.uniqueness='UNIQUE' and "
-              + "di.owner=dic.index_owner and di.index_name=dic.index_name  "
-              + "and not exists (select 1 from dba_constraints dc where dc.owner=di.owner and dc.CONSTRAINT_NAME=di.index_name and dc.CONSTRAINT_TYPE='P') "
-              + "and index_type='FUNCTION-BASED NORMAL' and 1=0"
-              + ") order by table_name,index_name, COLUMN_POSITION");
-      pstmt.setString(1, sourceSchema.toUpperCase());
-      rs = pstmt.executeQuery();
-      while (rs.next()) {
-        String tabName = rs.getString(1);
-        String indName = rs.getString(2);
-        String colName = rs.getString(3);
-        tab2Ind.put(indName, tabName);
-        log.info("table vs index found:" + tabName + " vs " + indName);
-        List<String> colList = cols2Ind.get(indName);
-        if (colList == null) {
-          colList = new ArrayList<String>();
-          cols2Ind.put(indName, colList);
-        }
-        colList.add(colName);
-
       }
       rs.close();
       pstmt.close();
@@ -87,7 +66,7 @@ public class IndexUtil {
           colString += colName + ",";
         }
         colString = colString.substring(0, colString.length() - 1);
-        String indexDDL = "create unique index " + targetSchema + "." + indName
+        String indexDDL = "create index " + targetSchema + "." + indName
             + " on " + targetSchema + "." + tabName + " (" + colString + ")";
         if (tableList.contains(tabName)) {
           log.info("constructed create index DDL :" + indexDDL);
@@ -111,6 +90,29 @@ public class IndexUtil {
       log.error(e);
       if (e.getErrorCode() != 261) {
       }
+    }
+  }
+
+  private static String translateSysNcToTableName(String owner,
+      String sysNcName, Connection targetConn, Connection sourceConn, String tabName)
+          throws SQLException {
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    try {
+      pstmt = sourceConn.prepareStatement(
+          "select DATA_DEFAULT from dba_tab_cols where owner = ? " + 
+          "and column_name = ? and table_name = ?");
+      pstmt.setString(1, owner.toUpperCase());
+      pstmt.setString(2, sysNcName.toUpperCase());
+      pstmt.setString(3, tabName.toUpperCase());
+      rs = pstmt.executeQuery();
+      if (rs.next()) {
+        return rs.getString(1);
+      }
+      return sysNcName;
+    } finally {
+      rs.close();
+      pstmt.close();
     }
   }
 
