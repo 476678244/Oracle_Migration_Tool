c5567649575b5b9830790cf8afc66d7eaeab4888
Zonghan Wu <a476678244@gmail.com>
2016-03-24 11:29:10 +0800

    scope and validation

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -27,5 +27,43 @@ angular.module('myApp.controllers', []).
 
 var oracleMigration = window.oracleMigration;
 oracleMigration.controller('ConfigController', ["$scope", function($scope) {
-	$scope.ip = "10.58.100.66";
+	$scope.sourceIp = "10.58.100.66";
+	$scope.sourceUsername = "sfuser";
+	$scope.sourcePassword = "sfuser";
+	$scope.sourceSID = "dbpool1";
+	$scope.sourceSchema = "sfuser_tree";
+
+	$scope.sourceValidateResult = 0;
+
+	$scope.targetIp = "10.58.100.66";
+	$scope.targetUsername = "sfuser";
+	$scope.targetPassword = "sfuser";
+	$scope.targetSID = "dbpool1";
+	$scope.targetSchema = "sfuser_real";
+
+	$scope.targetValidateResult = 0;
+
+	$scope.validate = function (source) {
+		if (source) {
+			if ($scope.sourceIp == "10.58.100.66") {
+				$scope.sourceValidateResult = 1;
+			} else {
+				$scope.sourceValidateResult = -1;
+			}
+		}
+	}
+
+	$scope.validateSuccess = function(result) {
+		if (result > 0) {
+			return true;
+		}
+		return false;
+	} 
+
+	$scope.validateFail = function(result) {
+		if (result < 0) {
+			return true;
+		}
+		return false;
+	} 
 }]);
