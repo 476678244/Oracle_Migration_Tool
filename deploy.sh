1e150eeb7030de2aa3dda19fc7a3a86afbf0b0eb
Zonghan Wu <a476678244@163.com>
2016-07-15 11:53:53 +0000

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd /local/tomcat/apache-tomcat-8.0.36/bin
+./shutdown.sh
+cd ../webapps
+rm -rf spring-based
+cd /local/git/spring-based
+git pull origin data_manage
+mvn install
+cp -r target/springbased-1.0 ../../tomcat/apache-tomcat-8.0.36/webapps/
+cd /local/tomcat/apache-tomcat-8.0.36/bin
+./startup.sh
