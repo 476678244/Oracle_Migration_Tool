f94267d455c30167cef0d6ecea6a9b237741a214
Zonghan(Wu Zonghan) <zwu@coupang.com>
2017-07-25 10:37:30 +0800

    multi insert via JDBCTemplate

--- src/main/java/springbased/example/dao/impl/JdbcVehicleDao.java
+++ src/main/java/springbased/example/dao/impl/JdbcVehicleDao.java
@@ -1,5 +1,8 @@
 package springbased.example.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import springbased.example.bean.Vehicle;
 import springbased.example.dao.VehicleDao;
 
@@ -22,7 +25,6 @@ public class JdbcVehicleDao implements VehicleDao {
 	}
 
 	public void insert(Vehicle vehicle) {
-
 		String sql = "INSERT INTO VEHICLE (VEHICLE_NO, COLOR, WHEEL, SEAT) " + "VALUES (?, ?, ?, ?)";
 		Connection conn = null;
 		try {
@@ -43,23 +45,50 @@ public class JdbcVehicleDao implements VehicleDao {
 				} catch (SQLException e) {
 				}
 			}
-
 		}
+	}
 
+	public void insert2(final Vehicle vehicle) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+				String sql = "INSERT INTO VEHICLE " + "(VEHICLE_NO, COLOR, WHEEL, SEAT) " + "VALUES (?, ?, ?, ?)";
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, vehicle.getVehicleNo());
+				ps.setString(2, vehicle.getColor());
+				ps.setInt(3, vehicle.getWheel());
+				ps.setInt(4, vehicle.getSeat());
+				return ps;
+			}
+		});
+	}
+
+	public void insert3(final Vehicle vehicle) {
+		String sql = "INSERT INTO VEHICLE (VEHICLE_NO, COLOR, WHEEL, SEAT) VALUES (?, ?, ?, ?)";
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(sql, new PreparedStatementSetter() {
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setString(1, vehicle.getVehicleNo());
+				ps.setString(2, vehicle.getColor());
+				ps.setInt(3, vehicle.getWheel());
+				ps.setInt(4, vehicle.getSeat());
+			}
+		});
+	}
+
+	public void insert4(final Vehicle vehicle) {
+		String sql = "INSERT INTO VEHICLE (VEHICLE_NO, COLOR, WHEEL, SEAT) VALUES (?, ?, ?, ?)";
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.update(sql, vehicle.getVehicleNo(),vehicle.getColor(),vehicle.getWheel(), vehicle.getSeat() );
 	}
 
 	public Vehicle findByVehicleNo(String vehicleNo) {
-
 		String sql = "SELECT * FROM VEHICLE WHERE VEHICLE_NO = ?";
 		Connection conn = null;
 		try {
-
 			conn = dataSource.getConnection();
-
 			PreparedStatement ps = conn.prepareStatement(sql);
-
 			ps.setString(1, vehicleNo);
-
 			Vehicle vehicle = null;
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
@@ -78,7 +107,6 @@ public class JdbcVehicleDao implements VehicleDao {
 				}
 			}
 		}
-
 	}
 
 	public void update(Vehicle vehicle) {/* ... */}
